06fb0e56d0680c70ef265c3e523cd09b0d207b91
zachglick <glickzachary@gmail.com>
2019-03-13 13:10:51 -0400

    cleanup

--- psi4/driver/procrouting/dboc.py
+++ psi4/driver/procrouting/dboc.py
@@ -38,6 +38,7 @@ amu_to_j = qcel.constants.get('atomic mass constant')
 au_to_amu = au_to_j / amu_to_j
 mass_e = qcel.constants.get('electron mass in u')
 
+# Helper class to collect DETCI info from C++
 class CIVect:
     
     def __init__(self, scf_wfn):
@@ -73,11 +74,9 @@ class CIVect:
             coeffs_block = self.ci_wfn.get_coeffs(dvec,root,block)
             if len(coeffs_block) is 0:
                 break
-
             # for each determinant, the alpha and beta string index that describes it
             str_inds_block = self.ci_wfn.get_string_inds(dvec,root,block)
-
-            # for all alpha and beta strings in this block, the orbital occupations defining the string
+            # for all alpha and beta strings in this block, the orbital occupations defining those strings
             occs_block = self.ci_wfn.get_string_occs(dvec,root,block)
             # first two indices give number of alpha strings, number of beta strings
             nstr_a_block = occs_block[0]
@@ -90,7 +89,6 @@ class CIVect:
             assert len(occs_block) == (len_a_block + len_b_block)
             occs_a_block = occs_block[:len_a_block]
             occs_b_block = occs_block[len_a_block:]
-
 
             coeffs.extend(coeffs_block)
             str_inds.extend(str_inds_block)
@@ -105,7 +103,7 @@ class CIVect:
         self.coeffs = np.array(coeffs)
 
         # some of the strings recorded in occs_a and occs_b may be duplicates
-        # get the max string index, which corresponds to the number of strings
+        # get the max alpha and beta string index, which must be the number of strings - 1
 
         occs_a = np.array(occs_a).reshape( (int(len(occs_a)/(self.na + 1)), self.na + 1 ) )
         occs_b = np.array(occs_b).reshape( (int(len(occs_b)/(self.nb + 1)), self.nb + 1 ) )
@@ -209,17 +207,19 @@ def calc_wfn_overlap(vec_m, S_mo, vec_p):
     mat_mb_pa_1 = vec_m.coeffs.T @ S_a
     mat_mb_pa_2 = S_b @ vec_m.coeffs.T
 
-    debug_mat(mat_mb_pa_1)
-    debug_mat(mat_mb_pa_2)
+    #debug_mat(mat_mb_pa_1)
+    #debug_mat(mat_mb_pa_2)
 
     mat = np.multiply(mat_mb_pa_1,mat_mb_pa_2)
     return np.sum(mat)
 
+# write matrix to file for debugging
 def debug_mat(mat):
     with open('DEBUG','a+') as f:
         f.write(np.array_str(np.round(mat,10), max_line_width=1000000, precision=5, suppress_small=False))
         f.write('\n')
 
+# write string to file for debugging
 def debug_str(string):
     with open('DEBUG','a+') as f:
         f.write(string)
@@ -227,7 +227,8 @@ def debug_str(string):
 
 def calc_dboc(wfn):
 
-    # DBOC only under specific input conditions (for now)
+    # DBOC only under specific input conditions
+    # Find a cleaner way to enforce this later
     if wfn.name() != 'RHF':
         raise Exception('Cannot do DBOC for {:s} wavefunction'.format(wfn.name()))
     if wfn.molecule().units() != 'Bohr':
@@ -240,21 +241,20 @@ def calc_dboc(wfn):
         raise Exception('Orientation must be fixed')
     
     # perform SCF at the original geometry
-    # save the location of the orbitals, we'll need to maintain them
+    # save the location of the orbitals, we'll use them as a guess throughout
     e_c, ref_wfn_c = driver.energy('scf',return_wfn=True)
     C_c = ref_wfn_c.Ca().to_array()
     psi_scratch = core.IOManager.shared_object().get_default_path()
     fname = os.path.split(os.path.abspath(core.get_writer_file_prefix(wfn.molecule().name())))[1]
     fname = os.path.join(psi_scratch, fname + ".180.npy")
 
-    # Finite Difference displacement
+    # finite difference displacement distance
     dR = 5.0e-4
     print("Performing DBOC with dR: ", dR, " a.u.")
-
     with open('DEBUG','w+') as f:
         f.write("Performing DBOC with dR: {:f}  a.u.\n".format(dR))
 
-    # Define these for convenience
+    # defined for convenience
     mints = core.MintsHelper(wfn)
     mol = wfn.molecule()
     init_geom = mol.geometry().to_array()
@@ -262,6 +262,7 @@ def calc_dboc(wfn):
     e_dboc = 0.0
     
     # generate a minus (-) and plus (+) displacement per nuclear coord
+    # calculate overlap of (-) and (+) to get dboc contribution of that coord
     for ai, atom in enumerate(init_geom):
 
         # The mass of the nucleus
@@ -269,8 +270,8 @@ def calc_dboc(wfn):
 
         for dim in range(3):
             
-            debug_str("\n\n\n~~~~~~~~~~~~~~~~~~~~coordinate {:d} of {:d}~~~~~~~~~~~~~~~~~~~~\n".format(3*ai+dim+1,3*len(init_geom)))
-            print("\n\ncoordinate {:d} of {:d}\n".format(3*ai+dim+1,3*len(init_geom)))
+            debug_str("\n\n\n~~~~~~~~~~~~~~~~~~~~nuclear coordinate {:d} of {:d}~~~~~~~~~~~~~~~~~~~~\n".format(3*ai+dim+1,3*len(init_geom)))
+            print("\n\nnuclear coordinate {:d} of {:d}\n".format(3*ai+dim+1,3*len(init_geom)))
 
             # Create the (-) displacement and get SCF orbitals
             geom_m = np.array(init_geom)
@@ -293,37 +294,42 @@ def calc_dboc(wfn):
             S_ao_mc = mints.ao_overlap(ref_wfn_m.basisset(),ref_wfn_c.basisset())
             S_ao_pc = mints.ao_overlap(ref_wfn_p.basisset(),ref_wfn_c.basisset())
             
-            # look at orbital energies to check for degeneracies
+            # look at orbital energies and record degenerate pairs
             eps_m = ref_wfn_m.epsilon_a().to_array()
             eps_p = ref_wfn_p.epsilon_a().to_array()
             degens = []
             for i in range(len(eps_m)-1):
-                if abs(eps_m[i] - eps_m[i+1]) > 1.0e-4 :
+                if abs(eps_m[i] - eps_m[i+1]) > 1.0e-5 :
                     continue
-                elif abs(eps_p[i] - eps_p[i+1]) > 1.0e-4 :
+                elif abs(eps_p[i] - eps_p[i+1]) > 1.0e-5 :
+                    print('THIS SHOULD NEVER HAPPEN')
                     continue
                 degens.append((i,i+1,))
 
 
-            S_mo = C_m.T @ S_ao_mp.np @ C_p 
-
+            # Get the overlap (in both AO and MO space) between (-) and (+) orbitals
+            #S_mo = C_m.T @ S_ao_mp.np @ C_p 
             #debug_str('\nMO Overlap between (-) and (+):\n')
             #debug_mat(S_mo)
             #debug_str('\nDiagonal:\n')
             #debug_mat(S_mo.diagonal())
+
+            # Align orbitals to maximize overlap
             print('Aligning (-)')
             C_m = align_orbs(C_m, C_c, S_ao_mc, degens)
             print('Aligning (+)')
             C_p = align_orbs(C_p, C_c, S_ao_pc, degens)
+            print('Aligning (-) with (+)')
+            C_m = align_orbs(C_m, C_p, S_ao_mp, degens)
 
             ref_wfn_m.set_Ca(core.Matrix.from_array(C_m))
             ref_wfn_p.set_Ca(core.Matrix.from_array(C_p))
 
-            # make a CIVect
+            # Do DETCI with SCF wavefunctions
             vec_m = CIVect(ref_wfn_m)
             vec_p = CIVect(ref_wfn_p)
 
-            # Make sure the reference determinant coefficients are in phase sign-wise
+            # Make sure the reference determinant coefficients are in phase
             if vec_m.coeffs[0,0] * vec_p.coeffs[0,0] < 0:
                 #print('Reversed the (-) CI coefficients')
                 vec_m.coeffs = -1 * vec_m.coeffs
@@ -332,16 +338,11 @@ def calc_dboc(wfn):
             if not ( np.array_equal(vec_m.occs_a,vec_p.occs_a) and np.array_equal(vec_m.occs_b,vec_p.occs_b) ):
                 raise Exception("CI Determinants in different order")
             
+            # Get the overlap between (-) and (+)
             S_mo = C_m.T @ S_ao_mp.np @ C_p 
-
-            #debug_str('\nRotated MO Overlap between (-) and (+):\n')
-            #debug_mat(S_mo)
-            #debug_str('\nRotated Diagonal:\n')
-            #debug_mat(S_mo.diagonal())
-            #print(np.round(S_mo.diagonal(),3))
-
             overlap_mp = calc_wfn_overlap(vec_m, S_mo, vec_p)
             
+            # Contribution of overlap to E_DBOC
             print("1 - <(+)|(-)>", 1 - overlap_mp )
             e_dboc_comp = (1-overlap_mp) / mass_nuc
             e_dboc += e_dboc_comp
